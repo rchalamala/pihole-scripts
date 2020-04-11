21b6a00bdd21fe7d5e563f47f606f14db327796d
HeroXLazer <HeroXLazer@users.noreply.github.com>
2020-04-11 16:19:35 -0600

    Update pihole.py

--- pihole.py
+++ pihole.py
@@ -2,7 +2,8 @@ import os
 
 os.system("sudo apt-get update -y")
 os.system("sudo apt-get upgrade -y")
-os.system("sudo apt autoremove -y")
+os.system("sudo apt-get autoremove -y")
+os.system("sudo apt-get autoclean -y")
 os.system("pihole -up")
 os.system("pihole -g")
 os.system("pihole restartdns")
